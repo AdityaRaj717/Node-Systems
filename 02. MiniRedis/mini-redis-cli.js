63edfae88bd9e5754d16e4ab65a0666ead9d4e69
AdityaRaj717 <rajaditya81795@gmail.com>
2026-02-11 23:07:09 +0530

    implement simple server side RESP parser

--- 02. MiniRedis/mini-redis-cli.js
+++ 02. MiniRedis/mini-redis-cli.js
@@ -1,93 +1,94 @@
-import net from 'node:net'
-import * as readline from 'node:readline'
+import net from "node:net";
+import * as readline from "node:readline";
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout, prompt: "mini-redis> " })
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "mini-redis> ",
+});
 
-// Convert into tokens 
+// Convert into tokens
 // Ex. ["SET", "key", "value"]
 function tokenizer(rawString) {
-  let tokens = []
-  let current = ""
-  let insideQuotes = false
+  let tokens = [];
+  let current = "";
+  let insideQuotes = false;
 
   for (let i = 0; i < rawString.length; i++) {
     if (rawString[i] === '"') {
-      insideQuotes = !insideQuotes
+      insideQuotes = !insideQuotes;
       if (insideQuotes === false) {
-        tokens.push(current)
-        current = ""
+        tokens.push(current);
+        current = "";
       }
-    }
-    else if (rawString[i] === ' ') {
-      if (insideQuotes) current += rawString[i]
+    } else if (rawString[i] === " ") {
+      if (insideQuotes) current += rawString[i];
       else if (current !== "") {
-        tokens.push(current)
-        current = ""
+        tokens.push(current);
+        current = "";
       }
-    }
-    else current += rawString[i]
+    } else current += rawString[i];
   }
 
-  if (insideQuotes) return null
-  else if (current !== "") tokens.push(current)
+  if (insideQuotes) return null;
+  else if (current !== "") tokens.push(current);
 
-  return tokens
+  return tokens;
 }
 
 // Encode into RESP (Redis Serialization Protocol) format
+// *3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
 function encodeRESP(tokens) {
-  let encoded = ""
-  encoded += `*${tokens.length}\r\n`
+  let encoded = "";
+  encoded += `*${tokens.length}\r\n`;
   for (let i = 0; i < tokens.length; i++) {
-    encoded += `$${Buffer.byteLength(tokens[i])}\r\n${tokens[i]}\r\n`
+    encoded += `$${Buffer.byteLength(tokens[i])}\r\n${tokens[i]}\r\n`;
   }
-  return encoded
+  return encoded;
 }
 
-const client = net.createConnection(6379, 'localhost', () => {
-  console.log('Connected to Server')
-  rl.prompt()
-})
-
-// Buffering 
-let buffer = ""
-client.on('data', (chunk) => {
-  buffer += chunk.toString()
-
-  while (buffer.includes('\r\n')) {
-    const index = buffer.indexOf('\r\n')
-    const message = buffer.slice(0, index)
-    buffer = buffer.slice(index + 2)
-    console.log(message)
-    rl.prompt()
-  }
-})
-
-rl.on('line', (line) => {
-  const rawData = line.toString().trim()
-  if (rawData.toLowerCase() === "exit") {
-    console.log('Goodbye!')
-    process.exit(0)
-  }
-
-  const tokens = tokenizer(rawData)
-  if (!tokens) {
-    console.log('Error (unmatched quote)')
-    rl.prompt()
-    return
-  }
-
-  const encodedRESP = encodeRESP(tokens)
-  client.write(encodedRESP)
-})
-
-rl.on('SIGINT', () => {
-  rl.question('Are you sure you want to exit? (y/n) ', (answer) => {
-    if (answer.match(/^y(es)?$/i)) {
-      console.log('Goodbye!')
-      process.exit(0);
-    }
-    else rl.prompt();
-  });
+const client = net.createConnection(6379, "localhost", () => {
+  console.log("Connected to Server");
+  rl.prompt();
 });
 
+// Buffering
+let buffer = "";
+client.on("data", (chunk) => {
+  buffer += chunk.toString();
+
+  while (buffer.includes("\r\n")) {
+    const index = buffer.indexOf("\r\n");
+    const message = buffer.slice(0, index);
+    buffer = buffer.slice(index + 2);
+    console.log(message);
+    rl.prompt();
+  }
+});
+
+rl.on("line", (line) => {
+  const rawData = line.toString().trim();
+  if (rawData.toLowerCase() === "exit") {
+    console.log("Goodbye!");
+    process.exit(0);
+  }
+
+  const tokens = tokenizer(rawData);
+  if (!tokens) {
+    console.log("Error (unmatched quote)");
+    rl.prompt();
+    return;
+  }
+
+  const encodedRESP = encodeRESP(tokens);
+  client.write(encodedRESP);
+});
+
+rl.on("SIGINT", () => {
+  rl.question("Are you sure you want to exit? (y/n) ", (answer) => {
+    if (answer.match(/^y(es)?$/i)) {
+      console.log("Goodbye!");
+      process.exit(0);
+    } else rl.prompt();
+  });
+});
--- 02. MiniRedis/server.js
+++ 02. MiniRedis/server.js
@@ -1,33 +1,77 @@
-import net from 'node:net'
+import net from "node:net";
+
+function tryParseRESP(buffer) {
+  let offset = 0;
+
+  if (buffer.length < 1) return null;
+  if (buffer[offset] !== "*") throw new Error("Protocol error: expected *");
+
+  const headerEnd = buffer.indexOf("\r\n", offset);
+  if (headerEnd === -1) return null;
+
+  let argCount = parseInt(buffer.slice(1, headerEnd), 10);
+  if (isNaN(argCount)) throw new Error("Protocol error: invalid array length");
+
+  let args = [];
+
+  offset = headerEnd + 2;
+
+  for (let i = 0; i < argCount; i++) {
+    if (buffer.length <= offset) return null;
+    if (buffer[offset] !== "$") throw new Error("Protocol error: expected $");
+
+    let bulkHeaderEnd = buffer.indexOf("\r\n", offset);
+    if (bulkHeaderEnd === -1) return null;
+
+    let bulkLen = parseInt(buffer.slice(offset + 1, bulkHeaderEnd), 10);
+    if (isNaN(bulkLen)) throw new Error("Protocol error: invalid bulk length");
+
+    offset = bulkHeaderEnd + 2;
+
+    if (buffer.length < offset + bulkLen + 2) return null;
+    const value = buffer.slice(offset, offset + bulkLen);
+
+    args.push(value);
+
+    offset += bulkLen + 2;
+  }
+
+  return {
+    args,
+    bytesConsumed: offset,
+  };
+}
 
 const server = net.createServer((socket) => {
-  console.log('Client connected.')
+  console.log("Client connected.");
 
-  let buffer = ""
-  socket.on('data', (chunk) => {
-    buffer += chunk.toString()
+  let buffer = "";
 
-    while (buffer.includes('\r\n')) {
-      const index = buffer.indexOf('\r\n')
-      const message = buffer.slice(0, index)
-      buffer = buffer.slice(index + 2)
+  socket.on("data", (chunk) => {
+    buffer += chunk.toString("utf8");
 
-      console.log(message)
-      socket.write('Hello from the server!\r\n')
+    while (true) {
+      const result = tryParseRESP(buffer);
+      if (!result) break;
+
+      const { args, bytesConsumed } = result;
+      buffer = buffer.slice(bytesConsumed);
+
+      // handleCommand(args);
     }
-  })
+  });
 
-  socket.on('end', () => {
-    console.log('Client has disconnected.')
-  })
+  socket.on("end", () => {
+    console.log("Client has disconnected.");
+  });
 
-  socket.on('error', (error) => {
-    console.log(`Client is facing some issues ${error.message}`)
-  })
-})
+  socket.on("error", (error) => {
+    console.log(`Client is facing some issues ${error.message}`);
+  });
+});
 
-const PORT = 6379
+const PORT = 6379;
 
 server.listen(PORT, () => {
-  console.log('Server has started...')
-})
+  console.log("Server has started...");
+});
